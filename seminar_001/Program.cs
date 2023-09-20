18a60aa33fe7ae21edde2ca87d19322d01f67882
Elena <knif-fly@mail.ru>
2023-09-20 18:25:03 +0300

    Добавили информацию в seminar_001/bin/ и seminar_001/obj/Debug/

--- seminar_001/Program.cs
+++ seminar_001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число
+// и выдает его квадрат (число умноженное
+// на само себя).
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int str = number * number;
+
+//Вывод построчно
+Console.Write("Квадрат числа ");
+Console.Write(number);
+Console.Write(" равен = ");
+Console.Write(str);
+Console.WriteLine();
+
+//Вывод с использованием знака $
+Console.WriteLine($"Квадрат числа {number} равен = {str}");
+
+//Вывод собращением к переменным
+Console.WriteLine("Квадрат числа {0} равен = {1}", number, str);
+
+//Использование библиотеки Math
+int sqr = Convert.ToInt32(Console.ReadLine());
+double sqr_2 = Math.Pow(sqr,2);
+Console.WriteLine("Квадрат числа {0} равен = {1}", sqr, sqr_2);
